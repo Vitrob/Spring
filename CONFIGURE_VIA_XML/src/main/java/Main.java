47960dd0d216a837f83de692678bd081a4b0c71d
Vitrob <robert90witek@gmail.com>
2022-05-27 12:16:29 +0200

    Added configuration without context.xml

--- CONFIGURE_VIA_XML/src/main/java/Main.java
+++ CONFIGURE_VIA_XML/src/main/java/Main.java
@@ -1,13 +1,26 @@
+import managers.BusStopManager;
 import managers.ConnectionAssistance;
+import managers.ConnectionManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:context.xml");
+        System.out.println("First try - using contect.xml:");
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:context.xml");
         ConnectionAssistance connectionAssistance = applicationContext.getBean(ConnectionAssistance.class);
-        System.out.println(connectionAssistance.getConnections());
+
+        System.out.println("Outcome: "+connectionAssistance.getConnections());
+
+        System.out.println();
+        System.out.println("Second cry - without contect.xml:");
+        ConnectionManager connectionManager = new ConnectionManager();
+        BusStopManager busStopManager = new BusStopManager();
+        connectionAssistance = new ConnectionAssistance(connectionManager, busStopManager);
+
+        System.out.println("Outcome: "+connectionAssistance.getConnections());
+
 
     }
 }
